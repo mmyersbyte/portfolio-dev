bf59e9555bf394ebc4aed3578247b0fa539973af
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-02-07 19:31:11 -0300

    portfolio exoplanets adicionado, responsivo e exclusão da página about

---
+++ script.js
@@ -0,0 +1,17 @@
+const starContainer = document.createElement('div');
+        starContainer.classList.add('starfall-background');
+        document.body.appendChild(starContainer);
+
+        function createStar() {
+            const star = document.createElement('div');
+            star.classList.add('star');
+            star.style.left = `${Math.random() * 100}%`; 
+            star.style.animationDuration = `${Math.random() * 2 + 1}s`; 
+            starContainer.appendChild(star);
+
+            setTimeout(() => {
+                star.remove();
+            }, 3000);
+        }
+
+        setInterval(createStar, 200);
